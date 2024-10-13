74c679733ec425ee95928a1169541d125df3aac1
BorjaFernandezMunoz <cumonzoidberg@gmail.com>
2024-10-13 20:56:16 +0200

    segunda vez que lo subo; sigo teniendo problemas con el return

--- lista_de_la_compra.py
+++ lista_de_la_compra.py
@@ -18,18 +18,14 @@ def lista_de_la_compra(menu):
     if type(menu) != dict:
         return f"Error: '{menu}' no es un diccionario"
 
-    for receta in menu:                                 # recorro las claves
+    for receta in menu:                               # recorro las claves
         for ingrediente in menu[receta]:                  # diccionario[clave] = valor; como en este caso son listas, los puedo recorrer.
             if ingrediente not in lista_compra:
                 lista_compra.append(ingrediente)
-
     return lista_compra
 
 
+menu_comida = {"Guacamole": ["cilantro fresco", "cebolla", "chile fresco", "ajo", "tomate", "aguacates maduros", "zumo de limón", "sal"], "Pimientos del piquillo rellenos": ["huevo duro", "palitos de cangrejo", "atún en aceite de oliva", "mayonesa", "pimientos del piquillo", "pimiento verde", "pimiento rojo", "cebolla", "vinagre de vino", "sal", "aceite de oliva virgen extra"], "Solomillo de cerdo agridulce": ["aceite de oliva", "ajo", "solomillo de cerdo", "cebolla", "manzana", "tomate concentrado", "calabacines", "brandy", "vino de Oporto", "ciruelas pasas", "orejones de albaricoque", "mostaza", "piñones tostados"], "Brownie de chocolate con helado de turrón": ["chocolate amargo 70 % cacao", "mantequilla sin sal", "azúcar", "huevos", "sal", "harina de trigo", "nueces"]}
 
-menu_comida = {"Guacamole": ["cilantro fresco", "cebolla", "chile fresco", "ajo", "tomate", "aguacates maduros", "zumo de limón", "sal"],  "Pimientos del piquillo rellenos": ["huevo duro", "palitos de cangrejo", "atún en aceite de oliva", "mayonesa", "pimientos del piquillo", "pimiento verde", "pimiento rojo", "cebolla", "vinagre de vino", "sal", "aceite de oliva virgen extra"], "Solomillo de cerdo agridulce": ["aceite de oliva", "ajo", "solomillo de cerdo", "cebolla", "manzana", "tomate concentrado",                                   "calabacines", "brandy", "vino de Oporto", "ciruelas pasas", "orejones de albaricoque", "mostaza", 
-                                         "piñones tostados"], "Brownie de chocolate con helado de turrón": ["chocolate amargo 70% cacao", "mantequilla sin sal", "azúcar", "huevos", "sal", "harina de trigo", "nueces"]}
-
-
-
-lista_de_la_compra(menu_comida)
+lista_de_la_compra(8)
+lista_de_la_compra(menu_comida) 
